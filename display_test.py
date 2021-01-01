72388d041716b82b9c654d8c7af5bddce81f2664
Thomas Dreja <11506568+tdreja@users.noreply.github.com>
2021-01-01 18:34:32 +0100

    feat(epaper): added code to epaper for testing

--- display_test.py
+++ display_test.py
@@ -2,44 +2,49 @@
 # -*- coding:utf-8 -*-
 
 import logging
-
-from drawcalendar import drawcalendar
-from waveshare_epd import epd7in5
 import time
-from PIL import Image,ImageDraw
+from datetime import datetime
+
+from PIL import Image, ImageDraw
+
+from drawcalendar.drawcalendar import draw_calendar
+from drawtoday.drawtoday import draw_today, full_height
+from waveshare_epd import epd7in5
 
 logging.basicConfig(level=logging.DEBUG)
 
 try:
     logging.info("epd7in5 Demo")
-    
+
     epd = epd7in5.EPD()
     logging.info("init and Clear")
     epd.init()
-    #epd.Clear()
-    
+    # epd.Clear()
+
     logging.info("read png file")
     Himage = Image.open('test.png')
     epd.display(epd.getbuffer(Himage))
     time.sleep(2)
 
-    logging.info("create drawcalendar2")
+    logging.info("create calendar")
     image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
     draw = ImageDraw.Draw(image)
-    drawcalendar.draw_calendar(draw, (0, 0), epd.height)
+    today = datetime.now()
+    draw_today(draw, (0, 0), epd.height, today)
+    draw_calendar(draw, (0, full_height), epd.height, today.date())
     epd.display(epd.getbuffer(image.rotate(angle=90, expand=1)))
     time.sleep(2)
-    
+
     logging.info("Goto Sleep...")
     epd.sleep()
     time.sleep(3)
-    
+
     epd.Dev_exit()
-    
+
 except IOError as e:
     logging.info(e)
-    
-except KeyboardInterrupt:    
+
+except KeyboardInterrupt:
     logging.info("ctrl + c:")
     epd7in5.epdconfig.module_exit()
     exit()
